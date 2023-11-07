4ba1a6d57ddb35ee90a6e9cb594996dcbfceb6e8
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-07 08:32:06 -0300

    colors

---
+++ src/components/Color/index.jsx
@@ -0,0 +1,5 @@
+const Color = ({ color }) => (
+    <div></div>
+)
+
+export default Color;
---
+++ src/components/ColorRender/index.jsx
@@ -0,0 +1,15 @@
+import Color from "../Color";
+
+const ColorRender = () => {
+    const color = [
+        '#DFFF00',
+        '#FFBF00',
+        '#FF7F50',
+        '#DE3163'
+    ];
+    return (
+        <Color color={color} />
+    )    
+}
+
+export default ColorRender;
