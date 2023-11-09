bfdb6a2232637481987deff08427f8bb2177bcaa
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-08 22:05:40 -0300

    create context

---
+++ src/contexts/color-context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react"
+
+const initialState = true;
+
+const ColorContext = createContext({
+    color: initialState
+})
+
+const ColorProvider = ({ children }) => {
+    const [color, setColor] = useState(initialState);
+
+    return <ColorContext.Provider value={{color}} >{children}</ColorContext.Provider>
+}
+
+const colorHook = () => {
+    return useContext(ColorContext);
+}
+
+export { colorHook, ColorProvider as default }
