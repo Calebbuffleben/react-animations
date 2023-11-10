9a9a625855ff18617b21e4b675f2ba7f4a913733
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-10 08:46:07 -0300

    form

---
+++ src/components/FormValidation/index.jsx
@@ -0,0 +1,9 @@
+const FormValidation = () => {
+    <form>
+        <input name="email" type="email" required />
+        <input name="password" type="password" required />
+        <input name="confirm-password" type="password" required />
+    </form>
+}
+
+export default FormValidation;
