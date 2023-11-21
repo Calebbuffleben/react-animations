2a618530f77cddf02913a34948739fec912a5623
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-21 08:36:13 -0300

    slice

--- src/reducers/LoginSlice.jsx
+++ src/reducers/LoginSlice.jsx
@@ -4,4 +4,19 @@ const initialState = {
     loggedUser: null,
 }
 
-const UserLoginSlice = createSlice({})
+const UserLoginSlice = createSlice({
+    name: 'userLogin',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.loggedUser = action.payload
+        },
+        logout: (state) => {
+            state.loggedUser = null
+        }
+    }
+});
+
+export const { login, logout } = UserLoginSlice.actions;
+
+export default UserLoginSlice.reducer;
