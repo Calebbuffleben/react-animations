c5548696cb7f34025607caa20f41afc03c736429
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-22 20:39:46 -0300

    dispatch

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../reducers/LoginSlice";
 
 const LoginForm = () => {
+    const dispatch = useDispatch();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleLogin = () => {
         const user = { username, password };
+        dispatch(login(user));
     }
 
     return (
