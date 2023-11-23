f1f61309960b6c10c55f29609431c5523532c84a
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-23 08:39:21 -0300

    provider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import HamburgerMenu from './components/HamburguerMenu';
 import ColorRender from './components/ColorRender';
 import DarkMode from './components/DarkMode';
 import FormValidation from './components/FormValidation';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
   return (
-    <div className="App">
-      <DarkMode>
-        <HamburgerMenu />
-        <ColorRender />
-        <FormValidation />
-        <br />
-      </DarkMode>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        
+        <DarkMode>
+          <HamburgerMenu />
+          <ColorRender />
+          <FormValidation />
+          <br />
+        </DarkMode>
+      </div>
+    </Provider>
   );
 }
 
