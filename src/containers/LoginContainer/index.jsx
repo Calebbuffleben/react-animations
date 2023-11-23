ef15e37db3d9a97e5efd2888ae829e5c8c327487
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-22 23:13:25 -0300

    login container

--- src/containers/LoginContainer/index.jsx
+++ src/containers/LoginContainer/index.jsx
@@ -1,20 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import LoginForm from "../../components/Login"
+import { logout } from "../../reducers/LoginSlice";
 
 const LoginContainer = () => {
     const loggedInUser = useSelector((state) => state.userLogin.loggedInUser);
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
 
     return (
-        <div>
-      <h1>User Login Page</h1>
-      {!loggedInUser ? (
-        <LoginForm />
-      ) : (
+    <div>
+        <h1>User Login Page</h1>
+        {!loggedInUser ? (
+            <LoginForm />
+        ) : (
         <div>
             <h2>User Information</h2>
-            <p>Username: {/*user.username*/}</p>
+            <p>Username: {loggedInUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
         </div>
-      )}
+        )}
     </div>
     )
 };
