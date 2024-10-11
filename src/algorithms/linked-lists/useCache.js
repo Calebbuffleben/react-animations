121ba2e34935ba7d4a67d65d1b7d3c03b79f3185
Calebbuffleben <calebbuffleben@gmail.com>
2024-10-11 17:57:41 -0300

    remove the last of the linked list

--- src/algorithms/linked-lists/useCache.js
+++ src/algorithms/linked-lists/useCache.js
@@ -9,7 +9,7 @@ class Cache{
     put(key, value){
         //If the cache arleady have this key
         if(cache[key]){
-            //Update the value of hte key and move it to front
+            //Update the value of the key and move it to front
             this.cache[key].value = value;
 
             this.moveToFront();
@@ -31,10 +31,14 @@ class Cache{
             value,
             next: null
         }
+
+        //If the first element is empty insert in the head
         if(!this.head){
             this.head = newNode;
             this.tail = newNode;
-        } else {
+        } 
+        // Otherwise will assign the value of the head for the next
+        else {
             newNode.next = this.head
             this.head = newNode;
         }
@@ -44,28 +48,56 @@ class Cache{
 
     moveToFront(key) {
         const current = this.cache[key];
-
+        
+        //If the values is equal to head, will return
         if (current === this.head) return;
 
         let prev = null;
         let node = this.head;
 
         while(node && node.key !== key){
+            //Move the current from the next position
             prev = node;
             node = node.next;
         }
 
         if(!node) return;
 
+        
         if(node === this.tail){
             this.tail = prev
         }
 
+        // Assign the value of the next to the previous "next"
         if(prev){
             prev.next = node.next
         }
 
+        //Assign the value of the head for the "next" pointer of the values and assign all the node from the head 
         node.next = this.head;
         this.head = node;
+    }
+
+    removeLast(){
+        if(!this.head) return;
+
+        const lastKey = this.tail.key;
+        delete this.cache[lastKey];
+
+        if(this.head === this.tail){
+            this.head = null;
+            this.tail = null;            
+        } else {
+            let current = this.head;
+
+            //Finding the element before the last
+            while(current.next !== this.tail){
+                current = current.next;
+            }
+
+            // Assign for the tail the new last value
+            current.next = null;
+            this.tail = current;
+        }
     }
 }
