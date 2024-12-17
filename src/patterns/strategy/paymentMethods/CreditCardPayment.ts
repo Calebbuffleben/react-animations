b50c9bde13d9cecc385bc1011d1c1e6d46d5fec6
Calebbuffleben <calebbuffleben@gmail.com>
2024-12-17 15:17:10 -0300

    add business rules to Credit Card Payment

--- src/patterns/strategy/paymentMethods/CreditCardPayment.ts
+++ src/patterns/strategy/paymentMethods/CreditCardPayment.ts
@@ -1,7 +1,29 @@
+import axios from "axios";
 import { IPaymentStrategy } from "../IPaymentStrategy";
 
 export class CreditCardPayment implements IPaymentStrategy {
-    pay(amount: number) {
-        return `Paid ${amount} using Credit Card.`;
+    private apiEndpoint = '/api/payments/credit-card'; // Backend API endpoint
+
+    async pay(amount: number) {
+        try {
+            const cardDetails = {
+                cardNumber: '000000000',
+                expiry: '12/3009',
+                cvv: '123'
+            };
+
+            const response = await axios.post(this.apiEndpoint, {
+                amount,
+                ...cardDetails,
+            });
+            if (response.data.status === 'success') {
+                return `Credit card payment successful! Transaction ID: ${response.data.transactionId}`;
+            } else {
+                throw new Error('Credit card payment failed.');
+            }
+        
+        } catch (error) {
+            throw new Error(`Credit card payment failed: ${error.message}`);
+        }
     }
 } 
